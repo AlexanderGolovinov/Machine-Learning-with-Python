4aa3d90e6648e44ace66194155092b37fdacc1a6
GOLO043 <aleksandr.goloinov@riverty.com>
2025-01-27 17:55:55 +0200

    Parameter Efficient Fine Tuning [PEFT]

---
+++ udacity/peft-project.py
@@ -0,0 +1,16 @@
+﻿# parameter-efficient fine-tuning using the Hugging Face peft library
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model
+
+# Load a pre-trained BERT model and tokenizer
+# MODEL_NAME = "bert-base-uncased"
+# tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+# model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
+
+# Load dataset (IMDB sentiment classification)
+# dataset = load_dataset("imdb")
+# train_data, test_data = dataset["train"], dataset["test"]
+dataset = load_dataset("imdb")
+print(dataset)
